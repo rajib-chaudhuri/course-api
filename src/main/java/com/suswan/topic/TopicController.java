751812785092c0c6a2c5f5a6825964d73d66be89
Suswan Mondal <suswan.mondal@wipro.com>
2019-03-20 11:16:07 +0530

    first commit

---
+++ src/main/java/com/suswan/topic/TopicController.java
@@ -0,0 +1,13 @@
+package com.suswan.topic;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TopicController {
+
+	@RequestMapping("/topics")
+	public String getAllTopics() {
+		return "All topics";
+	}
+}
