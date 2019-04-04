05a1cd52a4aca60915a7b6376a8561bd9062083d
Suswan Mondal <suswan.mondal@wipro.com>
2019-04-04 19:03:47 +0530

    changed jenkins file

--- stopContainer.sh
+++ stopContainer.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-if(docker rm CourseApiContainer >/dev/null 2>&1); then
-	echo "exist"
+if(docker stop CourseApiContainer >/dev/null 2>&1); then
+	echo "docker continaer stopped"	
 else
 	echo "not exist"	
 fi
