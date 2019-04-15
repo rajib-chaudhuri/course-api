d55d4a8c816bcb67285e1e4ce7d04b3e81bb7c54
suswan-mondal <suswan.mondal@wipro.com>
2019-04-15 18:31:45 +0530

    stop

--- stopContainer.sh
+++ stopContainer.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 applicationName=$1
-if(docker stop $applicationName'Container' >/dev/null 2>&1);
-fi
+
+if(docker stop $applicationName'Container' >/dev/null 2>&1); then
+	echo "$applicationName docker container stopped"	
+else
+	echo "not exist"	
