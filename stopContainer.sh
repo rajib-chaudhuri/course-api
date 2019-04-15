0a7dc6a0447cd6423a8885547b91a1a6e9964352
suswan-mondal <suswan.mondal@wipro.com>
2019-04-15 18:42:56 +0530

    stop

--- stopContainer.sh
+++ stopContainer.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 applicationName=$1
 echo "applicationName -->>> $applicationName"
-if(docker stop $applicationName'Container' >/dev/null 2>&1); then
-	echo "$applicationName docker container stopped"	
-else
-	echo "not exist"	
+
+
+if [ $1 =="suswan" ]
+then
+echo Hey that\'s a large number.
+if (( $1=="xxx"  ))
+then
+echo And is also an even number.
+fi
 fi
