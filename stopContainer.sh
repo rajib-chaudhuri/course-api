6242d78e93a65c464fd6c6249f588c1ca0f5301b
suswan-mondal <suswan.mondal@wipro.com>
2019-04-15 19:40:08 +0530

    stop

--- stopContainer.sh
+++ stopContainer.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 applicationName=$1
 echo "applicationName--->$applicationName"
-if(docker stop $applicationName'Container' >/dev/null 2>&1); then
+if(docker stop "$applicationName"'Container' >/dev/null 2>&1); then
 	echo "$applicationName docker container stopped"	
 else
 	echo "docker container could not be stopped ---not exist"	
 fi
 
-if(docker rm $applicationName'Container' >/dev/null 2>&1); then
+if(docker rm "$applicationName"'Container' >/dev/null 2>&1); then
 	echo "$applicationName docker container removed"	
 else
 	echo "docker container could not be removed ---not exist"	
