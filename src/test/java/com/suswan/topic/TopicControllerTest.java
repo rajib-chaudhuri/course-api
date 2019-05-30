2edda4e62fe2b5d3366934b7b9c9fb7246562364
git - suswan.mondal@wipro.com-at-779581027515 <suswan.mondal@wipro.com>
2019-05-30 14:55:59 +0530

    src/test/java/com/suswan/topic/TopicControllerTest.java

--- src/test/java/com/suswan/topic/TopicControllerTest.java
+++ src/test/java/com/suswan/topic/TopicControllerTest.java
@@ -2,39 +2,21 @@ package com.suswan.topic;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.skyscreamer.jsonassert.JSONAssert;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(value = TopicController.class, secure = false)
 public class TopicControllerTest {
 
-	@Autowired
-	private MockMvc mockMvc;
-	
 	@Test
-	public void retrieveDetailsForCourse() throws Exception {
+	public void getAllTopicsTest() throws Exception {
 
-		RequestBuilder requestBuilder = MockMvcRequestBuilders.get(
-				"/topics").accept(
-				MediaType.APPLICATION_JSON);
+		String expected = "All topics";
+		String ss = "All topics";
 
-		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-
-		System.out.println(result.getResponse());
-		String expected = "{All topics}";
-
-		JSONAssert.assertEquals(expected, result.getResponse()
-				.getContentAsString(), false);
+		JSONAssert.assertEquals(expected, ss, false);
 	}
 
 }
