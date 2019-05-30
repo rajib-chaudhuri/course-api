8103a8c35012d52ab4b93f394a78c3f7d9ccf510
git - suswan.mondal@wipro.com-at-779581027515 <suswan.mondal@wipro.com>
2019-05-30 16:11:35 +0530

    src/test/java/com/suswan/hello/HelloControllerTest.java

--- src/test/java/com/suswan/hello/HelloControllerTest.java
+++ src/test/java/com/suswan/hello/HelloControllerTest.java
@@ -1,5 +1,21 @@
 package com.suswan.hello;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(value = HelloController.class, secure = false)
 public class HelloControllerTest {
 
+	@Test
+	public void getAllTopicsTest() throws Exception {
+
+		String expected = "All topics";
+		String ss = "All topics";
+
+		JSONAssert.assertEquals(expected, ss, false);
+	}
 }
